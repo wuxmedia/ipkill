d17284b72e57189cae5440a0dbe3fb2c44f7745a
wuxmedia <wuxmedia@gmail.com>
2015-02-03 17:47:51 +0000

    Update ipkill.sh

--- ipkill.sh
+++ ipkill.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 #Subnet killer
 
-read IP
-echo "This IP is reportedly from:`geoiplookup $IP  | cut -d "," -f 2`!"
-WHODAT=`whois $IP`
-RANGE=`echo "$WHODAT" | grep inetnum: | cut -d ':' -f 2`
-echo $RANGE
-KILZN=`ipcalc $RANGE | tail -n 1`
+echo "This IP is reportedly from:$(geoiplookup $1  | cut -d "," -f 2)!"
+WHODAT=$(whois $1)
+RANGE=$(echo "$WHODAT" | grep -i -e "netrange:" -e "inetnum:" | cut -d ':' -f 2 | tail -n1)
+echo "Attempting to Black hole:$RANGE"!
+KILZN=$(ipcalc $RANGE | tail -n 1)
 echo "Last chance to hit Ctrl-C before I kill "$KILZN"?"
-sleep 3s
+sleep 1s
+echo "...3"
+sleep 1s
+echo "..2"
+sleep 1s
+echo ".1"
+sleep 1s
 route add -net $KILZN gw 127.0.0.1
 echo "$KILZN null-routed."
